7faa90a285693bbaa2182a7f9a6b707cc3d664a6
Ogawa <135859092+ogawa3427@users.noreply.github.com>
2023-11-07 23:49:17 +0900

    4Vに対応

--- app.py
+++ app.py
@@ -14,12 +14,13 @@ logger.setLevel(INFO)
 import discord
 import openai
 import deepl
+with open("config.json", "r", encoding="utf-8") as f:
+    config = json.load(f)
+model=config["model"]
+ignore_channel = config["ignore_channel"]
+import asyncio
 
-print(os.getenv("OHANASHI_CH"))
-data = { "hoge": "hoge", "fuga": "fuga" }
 
-with open("config.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -55,6 +56,24 @@ def sendlog(mess,timestamp,p,c):
     logger.info(string)
     print(string)
 
+
+async def get_completion(prompt):
+    # 非同期でAPIに問い合わせ
+    completion = await openai.AsyncCompletions.create(
+        engine=model,  # 使用するモデルを指定
+        prompt=prompt,
+        max_tokens=150  # 返答の最大トークン数を指定
+    )
+    return completion.choices[0].text.strip()
+
+async def main():
+    prompt = "こんにちは"
+    response = await get_completion(prompt)
+    print(response)
+
+    # 結果を出力
+   
+
 @client.event
 async def on_ready():
     logger.info("Start")
@@ -64,9 +83,9 @@ async def on_ready():
 async def on_message(message):
     print("RECIEVED")
     content = message.content
-    #if not message.channel.id == os.getenv("OHANASHI_CH"):
-    #    print("NOT OHANASHI_CH")
-    #    return
+    if message.channel.id == ignore_channel:
+        print("ignore")
+        return
     if message.author.bot:
         return
     else:
@@ -76,17 +95,13 @@ async def on_message(message):
         if client.user in message.mentions:
             content = re.sub(r'<@\d+>', '', content)
             print(content)
-            response = openai.ChatCompletion.create(
-                model="gpt-4",
+
+            # OpenAI API 非同期呼び出し
+            response = await openai.ChatCompletion.create(
+                model=model,
                 messages=[
-                    {
-                        "role": "system",
-                        "content": "nomal GPT4",
-                    },
-                    {
-                        "role": "user",
-                        "content": content,
-                    }
+                    {"role": "system", "content": "nomal GPT4"},
+                    {"role": "user", "content": content},
                 ],
             )
             ptokens = response['usage']['prompt_tokens']
@@ -94,34 +109,35 @@ async def on_message(message):
             restext = response['choices'][0]['message']['content']
 
             await message.channel.send(restext)
-            sendlog(restext,timestamp,ptokens,ctokens)
+            sendlog(restext, timestamp, ptokens, ctokens)
             return
         else:
-            result = translator.translate_text(message.content, target_lang="EN-US")
-            fromtranslog(result,timestamp)
+            # DeepL API 非同期呼び出し
+            result = await translator.translate_text(message.content, target_lang="EN-US")
+            fromtranslog(result, timestamp)
 
-            response = openai.ChatCompletion.create(
-                model="gpt-4",
+            # OpenAI API 非同期呼び出し
+            response = await openai.ChatCompletion.create(
+                model=model,
                 messages=[
-                    {
-                        "role": "system",
-                        "content": "nomal GPT4",
-                    },
-                    {
-                        "role": "user",
-                        "content": result.text,
-                    }
+                    {"role": "system", "content": "nomal GPT4"},
+                    {"role": "user", "content": result.text},
                 ],
             )
             ptokens = response['usage']['prompt_tokens']
             ctokens = response['usage']['completion_tokens']
             restext = response['choices'][0]['message']['content']
 
-            totranslog(restext,timestamp)
+            totranslog(restext, timestamp)
 
-            jaresult = translator.translate_text(restext, target_lang="JA")
+            # DeepL API 非同期呼び出し
+            jaresult = await translator.translate_text(restext, target_lang="JA")
+            sendlog(jaresult.text, timestamp, ptokens, ctokens)
             await message.channel.send(jaresult.text)
-            sendlog(jaresult.text,timestamp,ptokens,ctokens)
 
 
-client.run(TOKEN)
+
+#client.run(TOKEN)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
